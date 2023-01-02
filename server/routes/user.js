cdab8d1b6d29f155604cec71f6dd10a4bf04a707
DarGrz <102190066+DarGrz@users.noreply.github.com>
2023-01-02 20:06:15 +0100

    added user update functionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,31 @@
 import express from "express";
+import verifyToken from "./verifyToken.js";
+import { verifyTokenAndAuthorization } from "./verifyToken.js";
+import User from "../models/User.js";
+
 const router = express.Router();
 
-router.get("/router", (req, res) => {
-  res.send("Router is working fine");
-});
-router.post("/posttest", (req, res) => {
-  const userName = req.body.username;
-  console.log(userName);
-  res.send("Your user name is: " + userName);
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SEC_KEY
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json("Error editing user");
+  }
 });
 
 export default router;
