431c1e30097f2bc618bc507c2346fa5e99cd6649
DarGrz <102190066+DarGrz@users.noreply.github.com>
2023-01-05 21:41:30 +0100

    created routes: cart, product, order

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import userRouter from "./routes/user.js";
 import authRouter from "./routes/auth.js";
+import userRouter from "./routes/user.js";
+import productRouter from "./routes/product.js";
 
 const app = express();
 dotenv.config();
@@ -13,7 +14,7 @@ app.use(express.json());
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connection to mongoose succedsfull");
+    console.log("Connection to mongoose succesfull");
   })
   .catch((err) => {
     console.log(err);
@@ -23,8 +24,9 @@ app.get("/", (req, res) => {
   res.send("Success");
 });
 
-app.use("/", userRouter);
-app.use("/", authRouter); // REGISTER AND LOGIN
+app.use("/api/auth", authRouter); // REGISTER AND LOGIN
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running at port 5000");
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,4 +1,93 @@
 import express from "express";
+import Order from "../models/Order.js";
+import {
+  verifyToken,
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorization,
+} from "./verifyToken.js";
+
 const router = express.Router();
+
+//CREATE
+router.post("/", verifyToken, async (req, res) => {
+  const newOrder = new Order(req.body);
+
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (error) {
+    res.status(500).json("Error editing order");
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Order has been deleted" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET USER ORDERS
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const orders = await orders.find({ userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET All
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET MONTHLY INCOME
+
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const prevMonth = new Date(newDate.setMonth(lastMonth.getMonth() - 1));
+
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: prevMonth } } },
+      { $project: { month: { $month: "$createdAt" } } },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
